badec87b85e7539213e249444caea1e3343fbdc1
Administrator <Administrator@PC-20180428IHVI>
2020-07-15 00:02:35 +0800

    echo "# jkl" >> README.md git init git add README.md git commit -m "first commit" git remote add origin https://github.com/color-cn/jkl.git git push -u origin master

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	       My obj = (My) context.getBean("helloWorld");
+	       obj.getMessage();
+	       
+	}
+
+}
